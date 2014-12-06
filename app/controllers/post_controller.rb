de6845474628f4ef93bd34667940b8bea56e67b5
hitoshi.hasumi <hitoshi.hasumi@monstar-lab.com>
2014-12-06 10:29:28 +0900

    Bypassポッドを削除してWebView路線に戻した。 サーバからアプリ用の記事画面を返してもらう仕様に変更した

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -10,7 +10,7 @@ class PostController < UIViewController
     @webview = UIWebView.new.tap do |v|
       v.frame = self.view.bounds
       v.scalesPageToFit = true
-      v.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString("http://shimane.monstar-lab.com/#{@author_url_key}/#{@url_key}")))
+      v.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString("http://shimane.monstar-lab.com/#{@author_url_key}/#{@url_key}?mode=mobile_app")))
       v.delegate = self
       view.addSubview(v)
     end
