96b542e70e855e59df81bb13bafcc0c968b90624
hitoshi.hasumi <hitoshi.hasumi@monstar-lab.com>
2014-12-06 16:01:16 +0900

    その他ページをオートレイアウト化

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -5,38 +5,44 @@ class MenuController < UIViewController
     self.title = "その他"
     self.view.backgroundColor = UIColor.whiteColor
 
-    staff_button = UIButton.buttonWithType(UIButtonTypeSystem)
-    staff_button.setTitle("しまもんスタッフ", forState:UIControlStateNormal)
-    staff_button.addTarget(self, action:"show_staff", forControlEvents:UIControlEventTouchUpInside)
-    staff_button.frame = [[100, 100], [150, 32]]
-    staff_button.center = [self.view.frame.size.width / 2, self.view.frame.size.height / 2 - 170]
-    self.view.addSubview(staff_button)
+    @staff = UIButton.buttonWithType(UIButtonTypeSystem)
+    @staff.setTitle("しまもんスタッフ", forState:UIControlStateNormal)
+    @staff.addTarget(self, action:"show_staff", forControlEvents:UIControlEventTouchUpInside)
+    self.view.addSubview(@staff)
 
-    about_button = UIButton.buttonWithType(UIButtonTypeSystem)
-    about_button.setTitle("島根開発拠点について", forState:UIControlStateNormal)
-    about_button.addTarget(self, action:"show_contact", forControlEvents:UIControlEventTouchUpInside)
-    about_button.frame = [[100, 100], [150, 32]]
-    about_button.center = [self.view.frame.size.width / 2, self.view.frame.size.height / 2 - 100]
-    self.view.addSubview(about_button)
+    @contact = UIButton.buttonWithType(UIButtonTypeSystem)
+    @contact.setTitle("島根開発拠点について", forState:UIControlStateNormal)
+    @contact.addTarget(self, action:"show_contact", forControlEvents:UIControlEventTouchUpInside)
+    self.view.addSubview(@contact)
 
-    close_button = UIButton.buttonWithType(UIButtonTypeSystem)
-    close_button.setTitle("閉じる", forState:UIControlStateNormal)
-    close_button.addTarget(self, action:"close", forControlEvents:UIControlEventTouchUpInside)
-    close_button.frame = [[100, 100], [150, 32]]
-    close_button.center = [self.view.frame.size.width / 2, self.view.frame.size.height / 2 - 30]
-    self.view.addSubview(close_button)
+    @close = UIButton.buttonWithType(UIButtonTypeSystem)
+    @close.setTitle("閉じる", forState:UIControlStateNormal)
+    @close.addTarget(self, action:"close", forControlEvents:UIControlEventTouchUpInside)
+    self.view.addSubview(@close)
 
-    label = UILabel.alloc.initWithFrame([[0, 0], [0, 0]])
-    label.textColor = UIColor.grayColor
-    label.text = '本アプリは、RubyでネイティブiOSアプリ開発をするための仕組み「RubyMotion」を利用して制作されました'
-    label.sizeToFit
-    label.lineBreakMode = NSLineBreakByWordWrapping
-    label.numberOfLines = 0
-    label.setFrame([[0, 0], [self.view.frame.size.width - 100, label.frame.size.height]])
-    label.sizeToFit
-    label.center = [self.view.frame.size.width / 2, self.view.frame.size.height / 2 + 100]
-    self.view.addSubview(label)
+    @label = UILabel.new
+    @label.textColor = UIColor.grayColor
+    @label.text = '本アプリは、RubyでネイティブiOSアプリを開発できる「RubyMotion」を利用して制作されました'
+    @label.sizeToFit
+    @label.lineBreakMode = NSLineBreakByWordWrapping
+    @label.numberOfLines = 0
+    @label.setFrame([[0, 0], [self.view.frame.size.width - 100, @label.frame.size.height]])
+    @label.sizeToFit
+    self.view.addSubview(@label)
 
+    margin = Device.orientation.to_s.match(/\Alandscape/) ? 25 : 50
+    top = Device.orientation.to_s.match(/\Alandscape/) ? 70 : 150
+
+    Motion::Layout.new do |layout|
+      layout.view self.view
+      layout.metrics height: 32, margin: margin, top: top
+      layout.subviews staff: @staff, contact: @contact, close: @close, label: @label
+      layout.vertical "|-top-[staff(height)]-margin-[contact(height)]-margin-[close(height)]-100-[label(100)]-20-|"
+      layout.horizontal "|-40-[staff]-40-|"
+      layout.horizontal "|-40-[contact]-40-|"
+      layout.horizontal "|-40-[close]-40-|"
+      layout.horizontal "|-40-[label]-40-|"
+    end
   end
 
   def close
