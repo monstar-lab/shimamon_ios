c548b85754c0f4b7439ad6bd24daf67dcf0dbf96
HASUMI Hitoshi <hasumi.hitoshi@tecnicasamba.com>
2015-12-17 23:22:25 +0900

    UIWebViewにUserAgentを設定

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,6 +4,8 @@ class AppDelegate
     @index_controller = IndexController.alloc.initWithNibName(nil, bundle:nil)
     @window.rootViewController = UINavigationController.alloc.initWithRootViewController(@index_controller)
     @window.makeKeyAndVisible
+    # ユーザエージェントにshimamonを書くと、サーバがアプリ用のビューを返す
+    NSUserDefaults.standardUserDefaults.registerDefaults({UserAgent: "shimamon"})
     true
   end
 end
